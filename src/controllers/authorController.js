f740b367a45dbad0b805cab11368b639d02c43a3
ABHISHEK0186 <vinayak.abhishek11@gmail.com>
2022-03-15 01:32:44 +0530

    phase 1 complete

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -3,6 +3,7 @@ const AuthorModel= require("../models/authorModel")
 const createAuthor= async function (req, res) {
     try{
     let data= req.body
+    if(Object.keys(data)==0) return res.status(400).send({status:false, msg:"BAD REQUEST NO DATA PROVIDED"})
     let savedData= await AuthorModel.create(data)
     res.status(201).send({msg: savedData})
     }
@@ -14,10 +15,6 @@ const createAuthor= async function (req, res) {
     }
 }
 
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
-}
+
 
 module.exports.createAuthor= createAuthor
-module.exports.getUsersData= getUsersData
